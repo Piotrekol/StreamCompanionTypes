dbd21bd6e51ccc7a4e9582a8b0b6b23571e040f1
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2020-06-12 23:32:27 +0200

    Misc: Catch parser/formatter exceptions

--- StreamCompanionTypes/JaceEngine.cs
+++ StreamCompanionTypes/JaceEngine.cs
@@ -10,6 +10,7 @@ namespace StreamCompanionTypes
     public class JaceEngine
     {
         public static JaceEngine Instance = new JaceEngine();
+        public static string FormatErrorMessage = "One of the tokens is invalid";
         private readonly Regex _formulaMatcher = new Regex("\\{(.*?)\\}", RegexOptions.Compiled);
         private readonly Regex _formulaFormatMatcher = new Regex(" :(.*?)$", RegexOptions.Compiled);
         public CalculationEngine Engine { get; } = new CalculationEngine(CultureInfo.InvariantCulture, ExecutionMode.Compiled);
@@ -17,7 +18,6 @@ namespace StreamCompanionTypes
         private JaceEngine()
         {
         }
-
 
         public Dictionary<string, (string format, Func<IDictionary<string, double>, double> Func)> CompileFormulas(string formula)
         {
@@ -29,14 +29,24 @@ namespace StreamCompanionTypes
                 var textFormula = match.Groups[1].Value;
                 var formatMatch = _formulaFormatMatcher.Match(textFormula);
                 string format = string.Empty;
+
                 if (formatMatch.Success)
                 {
                     format = formatMatch.Groups[1].Value;
                     textFormula = textFormula.Replace($" :{format}", "");
                 }
-                var compiled = Engine.Build(textFormula);
-                var key = $"{{{textFormula}}}";
-                if (!result.ContainsKey(key))
+
+                Func<IDictionary<string, double>, double> compiled = DoubleNaN;
+                try
+                {
+                    compiled = Engine.Build(textFormula);
+                }
+                catch (Exception)
+                {
+                    format = string.Empty;
+                }
+
+                if (!result.ContainsKey(originalFormula))
                     result.Add(originalFormula, (format, compiled));
             }
 
@@ -48,10 +58,20 @@ namespace StreamCompanionTypes
         {
             foreach (var formula in compiledFormulas)
             {
-                textFormula = textFormula.Replace(formula.Key, formula.Value.Func(variables).ToString(formula.Value.format, CultureInfo.InvariantCulture));
+                try
+                {
+                    textFormula = textFormula.Replace(formula.Key,
+                        formula.Value.Func(variables).ToString(formula.Value.format, CultureInfo.InvariantCulture));
+                }
+                catch (VariableNotDefinedException)
+                {
+                    textFormula = textFormula.Replace(formula.Key, $"{{{FormatErrorMessage}}}");
+                }
             }
 
             return textFormula;
         }
+
+        private double DoubleNaN(IDictionary<string, double> z) => double.NaN;
     }
 }
