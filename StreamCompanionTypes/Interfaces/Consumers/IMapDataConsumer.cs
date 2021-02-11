bde5d86cc2812ac0a4b0834166a786fc49859d91
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2021-02-11 20:45:39 +0100

    Misc: More cancellation support

--- StreamCompanionTypes/Interfaces/Consumers/IMapDataConsumer.cs
+++ StreamCompanionTypes/Interfaces/Consumers/IMapDataConsumer.cs
@@ -1,9 +1,10 @@
-﻿using StreamCompanionTypes.DataTypes;
+﻿using System.Threading;
+using StreamCompanionTypes.DataTypes;
 
 namespace StreamCompanionTypes.Interfaces.Consumers
 {
     public interface IMapDataConsumer
     {
-        void SetNewMap(IMapSearchResult map);
+        void SetNewMap(IMapSearchResult map, CancellationToken cancellationToken);
     }
 }
--- StreamCompanionTypes/Interfaces/IMapDataFinder.cs
+++ StreamCompanionTypes/Interfaces/IMapDataFinder.cs
@@ -1,11 +1,12 @@
-﻿using StreamCompanionTypes.DataTypes;
+﻿using System.Threading;
+using StreamCompanionTypes.DataTypes;
 using StreamCompanionTypes.Enums;
 
 namespace StreamCompanionTypes.Interfaces
 {
     public interface IMapDataFinder
     {
-        IMapSearchResult FindBeatmap(IMapSearchArgs searchArgs);
+        IMapSearchResult FindBeatmap(IMapSearchArgs searchArgs, CancellationToken cancellationToken);
         OsuStatus SearchModes { get; }
         string SearcherName { get; }
         int Priority { get; set; }
