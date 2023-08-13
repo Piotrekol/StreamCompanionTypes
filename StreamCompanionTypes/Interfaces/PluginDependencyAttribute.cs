a0e15effdd688f661fa5520d17fab091e49dd3d9
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2023-08-13 21:58:43 +0200

    Add: PluginDependency attribute

---
+++ StreamCompanionTypes/Interfaces/PluginDependencyAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StreamCompanionTypes.Interfaces
+{
+    public class PluginDependencyAttribute : Attribute
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pluginName">Case sensetive name of the plugin</param>
+        /// <param name="semVerVersion">major.minor[.build[.revision]] eg. 1.0.0</param>
+        public PluginDependencyAttribute(string pluginName, string minVersion)
+        {
+            PluginName = pluginName;
+            MinVersion = Version.Parse(minVersion);
+        }
+
+        public string PluginName { get; }
+        public Version MinVersion { get; }
+    }
+}
